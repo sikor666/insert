84db3477134a40a4c17ed17da4b7a6aba41fcb70
Tomek <sikor666@gmail.com>
2019-02-24 15:09:28 +0100

    task 3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-﻿#include "task2.hpp"
+﻿#include "task3.hpp"
 
 #include <iostream>
 
 int main()
 {
-    cout << solution(3) << endl;
+    vector<int> ranks1 { 3, 4, 3, 0, 2, 2, 3, 0, 0 };
+    vector<int> ranks2 { 4, 2, 0 };
+    vector<int> ranks3 { 4, 4, 3, 3, 1, 0 };
+    vector<int> ranks4 { 4 };
+    vector<int> ranks5 { 4, 3 };
+    vector<int> ranks6 { 4, 2 };
+    vector<int> ranks7;
+
+    cout << solution(ranks1) << endl;
+    cout << solution(ranks2) << endl;
+    cout << solution(ranks3) << endl;
+    cout << solution(ranks4) << endl;
+    cout << solution(ranks5) << endl;
+    cout << solution(ranks6) << endl;
+    cout << solution(ranks7) << endl;
 
     return 0;
 }
---
+++ src/task3.hpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> &ranks)
+{
+    if (ranks.empty())
+    {
+        return 0;
+    }
+
+    sort(begin(ranks), end(ranks));
+
+    int result = 0;
+    int currentRank = ranks[ranks.size() - 1];
+    int previousRank = ranks[ranks.size() - 1];
+
+    for_each(rbegin(ranks), rend(ranks), [&](int r)
+    {
+        if (r < currentRank && currentRank < previousRank)
+        {
+            previousRank = currentRank;
+        }
+
+        if (r + 1 == previousRank)
+        {
+            result++;
+        }
+
+        if (r < currentRank)
+        {
+            currentRank = r;
+        }
+    });
+
+    return result;
+}
